a4ca60221f21159bc3e485af2b05b8e6d8acaac3
habanzu <habanzu@gmai.com>
2017-07-23 14:17:58 +0200

    Commit Test of Pycharm.

--- test_solver.py
+++ test_solver.py
@@ -13,8 +13,11 @@ class TestSolver(unittest.TestCase):
     def test_two_roots_2(self):
         self.assertEqual(Solver.demo(2, 5, 3), (-1.5, -1))
 
-    def test_one_root(self):
+    def test_one_root1(self):
         self.assertEqual(Solver.demo(15, 0, 0), 0)
+
+    def test_one_root2(self):
+        self.assertEqual(Solver.demo(1, 0, 0), 0)
 
     def test_no_roots(self):
         self.assertIsNone(Solver.demo(15, 7, 15))
